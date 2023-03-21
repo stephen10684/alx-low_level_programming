bfe280add89c39c9ecf6bc35a07359b6dca16465
stephen10684 <stepupsteve1@gmail.com>
2023-03-21 15:18:51 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -14,6 +14,5 @@ void print_alphabet(void)
 		_putchar(i);
 	}
 	_putchar('\n');
-}
-                                                                                                                                   
-~           
+}                                                                                                                             
+           
